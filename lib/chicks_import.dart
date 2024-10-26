d0d1e70cdd9fff57d8d41998f2854db7fcdaa3db
adilthengilan <adilthengilan123@gmail.com>
2024-10-26 22:59:10 +0530

    c

---
+++ lib/chicks_import.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChicksImport extends StatefulWidget {
+  const ChicksImport({super.key});
+
+  @override
+  State<ChicksImport> createState() => _ChicksImportState();
+}
+
+class _ChicksImportState extends State<ChicksImport> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
