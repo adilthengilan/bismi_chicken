9c04c8da08bf3f12d3c93e9599fabced97a35226
Naheelj <naheeljnaheel5454@gmail.com>
2024-10-27 16:49:55 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:bismi_chicken/dashboard.dart';
+import 'package:bismi_chicken/export.dart';
+import 'package:bismi_chicken/feeding.dart';
 import 'package:bismi_chicken/view_model.dart/dashboard_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -31,12 +33,12 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  // final List<Widget> pages = [
-  //   DashBoard(),
-  //   const Import(),
-  //   const Feed(),
-  //   const Export(),
-  // ];
+  final List<Widget> pages = [
+    DashBoard(),
+    // const Import(),
+    const Feed(),
+    const Export(),
+  ];
 
   HomePage({super.key});
 
